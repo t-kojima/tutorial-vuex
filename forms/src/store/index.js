dc1bb5ba1eba35b3f44def08b151c611ce47d400
t-kojima <t-kojima@skullware.net>
2018-06-25 23:27:54 +0900

    add: store#Head

--- forms/src/store/index.js
+++ forms/src/store/index.js
@@ -10,12 +10,36 @@ const Form = {
   actions: {
     buttonAction ({ commit, state, rootState }) {
       console.log('buttonAction')
+      commit('setStepCount', null, { root: true })
+    }
+  }
+}
+
+const Head = {
+  state: {
+    title: ['感想を入力', '確認画面', '送信完了']
+  },
+  mutation: {},
+  actions: {},
+  getters: {
+    getTitle (state, getters, rootState) {
+      return state.title[rootState.stepCount]
     }
   }
 }
 
 export default new Vuex.Store({
+  state: {
+    stepCount: 0
+  },
+  mutations: {
+    setStepCount (state) {
+      console.log('root set Step Count')
+      ++state.stepCount
+    }
+  },
   modules: {
-    Form
+    Form,
+    Head
   }
 })
